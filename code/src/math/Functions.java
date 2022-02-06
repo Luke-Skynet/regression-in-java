4f2da806f156c22303f3959ae452b3a8edee0171
Luke-Skynet <LucasYuan297@gmail.com>
2022-02-06 13:12:39 -0600

    added neural network dir and funct derivs

--- code/src/math/Functions.java
+++ code/src/math/Functions.java
@@ -7,7 +7,7 @@ public class Functions {
 	public static float sigmoid(float x) {
 		return 1 / (float) (1 + Math.exp(-x));
 	}
-	
+
 	public static Vector sigmoid(Vector that) {
 		
 		Vector vector = new Vector(that.getLength());
@@ -18,7 +18,21 @@ public class Functions {
 		
 		return vector;
 	}
-	
+
+	public static float derSigmoid(float x){
+		return sigmoid(x) * (1.0f - sigmoid(x));
+	}
+
+	public static Vector derSigmoid(Vector that){
+		Vector vector = new Vector(that.getLength());
+		
+		for (int i = 0; i < vector.getLength(); i++) {
+			vector.setValue(i, derSigmoid(that.getValue(i)));
+		}
+		
+		return vector;
+	}
+
 	//Tanh Function (individual and Vector)
 	
 	public static float tanh(float x) {
@@ -35,7 +49,22 @@ public class Functions {
 		
 		return vector;
 	}
-	
+
+	public static float derTanh(float x) {
+		return (float) (1.0 - Math.pow(Math.tanh(x), 2));
+	}
+
+	public static Vector derTanh(Vector that) {
+		
+		Vector vector = new Vector(that.getLength());
+		
+		for (int i = 0; i < vector.getLength(); i++) {
+			vector.setValue(i, derTanh(that.getValue(i)));
+		}
+		
+		return vector;
+	}
+
 	//ReLU Function (individual and Vector)
 	
 	public static float ReLU(float x) {
@@ -52,7 +81,26 @@ public class Functions {
 		
 		return vector;
 	}
-	
+
+	public static float derReLU(float x){
+		if (x >= 0){
+			return 1.0f;
+		} else {
+			return 0.0f;
+		}
+	}
+
+	public static Vector derReLU(Vector that) {
+		
+		Vector vector = new Vector(that.getLength());
+		
+		for (int i = 0; i < vector.getLength(); i++) {
+			vector.setValue(i, derReLU(that.getValue(i)));
+		}
+		
+		return vector;
+	}
+
 	//Swish Function (individual and Vector)
 	
 	public static float swish(float x) {
@@ -69,7 +117,22 @@ public class Functions {
 		
 		return vector;
 	}
-	
+
+	public static float derSwish(float x) {
+		return sigmoid(x) + swish(x)*(1.0f - sigmoid(x));
+	}
+
+	public static Vector derSwish(Vector that) {
+		
+		Vector vector = new Vector(that.getLength());
+		
+		for (int i = 0; i < vector.getLength(); i++) {
+			vector.setValue(i, derSwish(that.getValue(i)));
+		}
+		
+		return vector;
+	}
+
 	//Softmax Function (just Vector)
 	
 	public static Vector softMax(Vector that) {
@@ -90,6 +153,25 @@ public class Functions {
 		}
 		
 		return vector;
+	}
+
+	public static Matrix derSoftMax(Vector that){
+
+		int dims = that.getLength();
+		Matrix matrix = new Matrix(dims, dims);
+
+		Vector sm = softMax(that);
+
+		for(int i = 0; i < dims; i++){
+			for (int j = 0; j < dims; j++){
+				if (i == j){
+					matrix.setValue(i, i, sm.getValue(i) * (1.0f - sm.getValue(i)));
+				} else {
+					matrix.setValue(i, j, -1.0f * sm.getValue(i) * sm.getValue(j));
+				}
+			}
+		}
+		return matrix;
 	}
 
 	public static void main(String[] args) {
